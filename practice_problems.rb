25a724fc2060dee0970692a7e1f47e7cbba5b270
Elif Sezgin <elifmsezgin@gmail.com>
2017-03-16 11:26:01 -0700

    Find complement

--- practice_problems.rb
+++ practice_problems.rb
@@ -1,3 +1,4 @@
+# Hamming Question
 # The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
 #
 # Given two integers x and y, calculate the Hamming distance.
@@ -22,4 +23,23 @@ def hamming_distance(x, y)
         x, y = x/2, y/2
     end
     return distance
+end
+# Number Complement
+# Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+#
+# Note:
+# The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+# You could assume no leading zero bit in the integer’s binary representation.
+# Input: 5
+# Output: 2
+# Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+def find_complement(num)
+    comp = 0
+    factor = 0
+    until num == 0
+        comp += (2**factor) * (1 - (num % 2))
+        factor += 1
+        num /= 2
+    end
+    comp
 end
